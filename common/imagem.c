ee8d18b2b6cf50dc6f0041b4ebc958b2535c4e2d
Naylor Garcia <naylorgarcia@gmail.com>
2015-11-27 10:35:39 -0200

    Correcoes

--- common/imagem.c
+++ common/imagem.c
@@ -396,7 +396,7 @@ void applySmooth(initialParams* ct, PPMImageParams* imageParams, PPMThread* thre
 
                     // SOMA APENAS SE NAO FOR PIXEL DE BORDA
                     // SE FOR, A SOMO SERA EQUIVALENTE A ZERO
-                    if (l >= 0) {
+                    if (l > 2) {
                         p = (l-l2*imageParams->coluna)+c2;
                         if (strcmp(imageParams->tipo, "P6")==0) {
                             sumb += thread[numThread].ppmIn[p].blue;
