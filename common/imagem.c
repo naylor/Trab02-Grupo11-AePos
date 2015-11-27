e4791cd44e795cf8b2ef0f5856a3b0296297295c
Naylor Garcia <naylorgarcia@gmail.com>
2015-11-27 09:41:20 -0200

    Correcoes

--- common/imagem.c
+++ common/imagem.c
@@ -380,7 +380,7 @@ void applySmooth(initialParams* ct, PPMImageParams* imageParams, PPMThread* thre
     // FORAM LIDAS, COMECAR ENTAO A APLICACAO
     // DO SMOOTH APOS ELAS
     if (thread[numThread].li != 0)
-        inicio = 2;
+        inicio = 0;
 
     // PERCORRENDO OS PIXELS DO
     // PEDACO DA IMAGEM LIDA
@@ -399,7 +399,7 @@ void applySmooth(initialParams* ct, PPMImageParams* imageParams, PPMThread* thre
                     // SOMA APENAS SE NAO FOR PIXEL DE BORDA
                     // SE FOR, A SOMO SERA EQUIVALENTE A ZERO
                     if (l2 >= 0 && c2 >= 0) {
-                        p = (l2*imageParams->linha)+c2;
+                        p = (l2*imageParams->coluna)+c2;
                         if (strcmp(imageParams->tipo, "P6")==0) {
                             sumb += thread[numThread].ppmIn[p].blue;
                             sumg += thread[numThread].ppmIn[p].green;
