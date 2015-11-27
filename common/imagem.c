8913f33e226d103de56bc32dfe7484cab6c46645
Naylor Garcia <naylorgarcia@gmail.com>
2015-11-27 14:09:33 -0200

    Correcoes

--- common/imagem.c
+++ common/imagem.c
@@ -160,7 +160,7 @@ PPMThread* getDivisionThreads(initialParams* ct, PPMImageParams* imageParams, PP
     numElementos = (int)linhas/ct->numThreads;
 
         if (ct->debug >= 2)
-            printf("lf:%d\n", proxLinha);
+            printf("lf:%d\n", node[numNode].li);
 
     // COMECA A DIVISAO POR LINHA
     // MELHOR PARA CALCULAR 5x5 (NECESSARIO LER MENOS PIXELS)
