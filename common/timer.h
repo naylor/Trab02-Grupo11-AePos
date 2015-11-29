edaa748c6f426a0eefc649f65b49392651fcb456
Naylor Garcia <naylorgarcia@gmail.com>
2015-11-29 21:21:42 -0200

    Correcoes

--- common/timer.h
+++ common/timer.h
@@ -10,13 +10,13 @@
 		struct timeval timeval_start, timeval_end;
 		struct timespec timespec_start, timespec_end;
 		clock_t clock_start, clock_end, clock_diff;
-		double timeval_diff, timespec_diff, clock_diff_time;
+		double timeval_diff, timeval_diff_s, timeval_diff_u, timespec_diff, clock_diff_time;
 	} timer;
 #else
 	typedef struct timer_t {
 		struct timeval timeval_start, timeval_end;
 		clock_t clock_start, clock_end, clock_diff;
-		double timeval_diff, timespec_diff, clock_diff_time;
+		double timeval_diff, timeval_diff_s, timeval_diff_u, timespec_diff, clock_diff_time;
 	} timer;
 #endif
 
