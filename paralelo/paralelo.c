e8c33f6751852182811c048b8fb6630b3d82e41a
Naylor Garcia <naylorgarcia@gmail.com>
2015-11-29 16:42:01 -0200

    Correcoes

--- paralelo/paralelo.c
+++ paralelo/paralelo.c
@@ -85,7 +85,7 @@ PPMThread* paraleloNodeReadAndSmooth(initialParams* ct, PPMImageParams* imagePar
             {
                 stop_timer(tempoS);
             }
-                printf("[Time Smooth] %.2fms\n",tempoS.timeval_diff);
+                printf("[Time Smooth] %.2fms\n",tempoS->timeval_diff);
 
         }
         #pragma omp barrier
