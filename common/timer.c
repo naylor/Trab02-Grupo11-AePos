5100be893d612f500b44b67358e0586ea3afbab4
Naylor Garcia <naylorgarcia@gmail.com>
2015-11-29 14:01:26 -0200

    Correcoes

--- common/timer.c
+++ common/timer.c
@@ -23,7 +23,7 @@ double total_timer(timer* t) {
     t->timeval_diff = t->timeval_diff_s * 1000.0; // sec to ms
     t->timeval_diff += t->timeval_diff_u / 1000.0; // us to ms
 
-    return t->timeval_diff;
+    return scanf ("%.f",t->timeval_diff);
 }
 
 void show_timer(tempo* t, int numNodes) {
