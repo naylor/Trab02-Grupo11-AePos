6f9e3d5cf92c415dd80bce3f23259cdf53c4b705
Naylor Garcia <naylorgarcia@gmail.com>
2015-11-29 12:08:18 -0200

    Correcoes

--- paralelo/main.c
+++ paralelo/main.c
@@ -280,7 +280,7 @@ int main (int argc, char **argv){
                     //E AGUARDO POR MAIS TRABALHO
                     total_timer(relogio, tempoA, tempoR, tempoS, tempoW, rank);
 
-                printf("RRR: %.f\n", tempoR);
+                printf("RRR: %.f\n", relogio[rank].tempoR);
 
                     MPI_Ssend(&relogio[rank].tempoR, 1, MPI_FLOAT, 0, 15, MPI_COMM_WORLD);
                     MPI_Ssend(&relogio[rank].tempoS, 1, MPI_FLOAT, 0, 16, MPI_COMM_WORLD);
