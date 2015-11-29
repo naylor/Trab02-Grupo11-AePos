fe945b648581fb207b147652cc886b9aa63481af
Naylor Garcia <naylorgarcia@gmail.com>
2015-11-29 10:49:01 -0200

    Correcoes

--- paralelo/main.c
+++ paralelo/main.c
@@ -81,7 +81,7 @@ int main (int argc, char **argv){
     MPI_Bcast ( &ct->numThreads, inteiro, MPI_INT, 0, MPI_COMM_WORLD );
     MPI_Bcast ( &ct->debug, inteiro, MPI_INT, 0, MPI_COMM_WORLD );
 
-    relogio[rank] = (tempoParams* )malloc(sizeof(tempoParams));
+    relogio[rank] = (tempo* )malloc(sizeof(tempoParams));
 
     if (rank == 0) {
         if (ct->filePath != NULL) {
