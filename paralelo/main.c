57bc219df97c10dcc69387a10c46351a8441d835
Naylor Garcia <naylorgarcia@gmail.com>
2015-11-29 12:28:31 -0200

    Correcoes

--- paralelo/main.c
+++ paralelo/main.c
@@ -279,6 +279,9 @@ int main (int argc, char **argv){
                     //INFORMA O NODE QUE ACABOU
                     //E AGUARDO POR MAIS TRABALHO
                     total_timer(relogio, tempoA, tempoR, tempoS, tempoW, rank);
+
+                printf("RRR: %d\n", relogio[rank].tempoW);
+
                     MPI_Ssend(&relogio[rank].tempoR, 1, MPI_DOUBLE, 0, 15, MPI_COMM_WORLD);
                     MPI_Ssend(&relogio[rank].tempoS, 1, MPI_DOUBLE, 0, 16, MPI_COMM_WORLD);
                     MPI_Ssend(&relogio[rank].tempoW, 1, MPI_DOUBLE, 0, 17, MPI_COMM_WORLD);
