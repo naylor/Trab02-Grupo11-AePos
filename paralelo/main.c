8368d17124e1c89f52c80325a897a67bd5874e1f
Naylor Garcia <naylorgarcia@gmail.com>
2015-11-29 13:44:23 -0200

    Correcoes

--- paralelo/main.c
+++ paralelo/main.c
@@ -208,6 +208,8 @@ int main (int argc, char **argv){
 
             total_timer(relogio, rank, tempoA);
 
+            printf("[Time Read] %.0fms\n", relogio[1].tempoS);
+
             show_timer(relogio, ct->numProcessos);
 
             //ESCREVE NO ARQUIVO DE LOGS
